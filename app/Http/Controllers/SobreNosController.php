38dd1967e1ca51a11f04d298854e4a4552742441
Eduardo Esteves <eduardostevesdasilva@hotmail.com>
2022-06-03 21:07:48 -0300

    Criado as tres novas views bem basica so para teste mesmo, e em seus respectivos controller ao inves de escrever um texto eu passo a responsabilidade agora para a funcao view() que internamente o Laravel sabera resolver o caminho e carregar o arquivo de template

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class SobreNosController extends Controller
 {
     public function index() {
-        echo 'Sobre nos';
+        return view('site.sobre-nos');
     }
 }
