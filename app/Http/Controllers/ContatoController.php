dd983166380d43e896eb8829f194ca80d5e0c28a
Eduardo Esteves <eduardostevesdasilva@hotmail.com>
2022-11-14 19:00:18 -0300

    changed of array for compact method

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -7,9 +7,6 @@ use Illuminate\Http\Request;
 class ContatoController extends Controller
 {
     public function index(string $user, string $password) {
-        return view('site.contato', [
-            'user'      => $user,
-            'password'  => $password,
-        ]);
+        return view('site.contato', compact('user', 'password'));
     }
 }
