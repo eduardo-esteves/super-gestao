f274bf7f4f2a5434355667d3dffb83bb82cfd457
Eduardo Esteves <eduardostevesdasilva@hotmail.com>
2022-11-17 17:47:03 -0300

    edited views on default english name, improving views

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -7,23 +7,6 @@ use Illuminate\Http\Request;
 class SobreNosController extends Controller
 {
     public function index() {
-        $products = [
-            0 => [
-                'name'      => 'Prato',
-                'maker'     => 'Gall',
-                'price'     => 12.4
-            ],
-            1 => [
-                'name'      => 'Chinelo',
-                'maker'     => 'Matter',
-                'price'     => 32.4
-            ],
-            2 => [
-                'name'      => 'Camisa',
-                'maker'     => 'Metteor',
-                'price'     => 42.4
-            ],
-        ];
-        return view('site.sobre-nos', compact('products'));
+        return view('site.about');
     }
 }
--- resources/views/site/contact.blade.php
+++ resources/views/site/contact.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <title>Super Gestão - Sobre Nós</title>
+    <title>Super Gestão - Contato</title>
     <meta charset="utf-8">
 
     <style>
@@ -214,50 +214,51 @@
     </div>
 </div>
 
-<div class="conteudo-destaque">
-
-    <div class="esquerda">
-        <div class="informacoes">
-            <h1>Sistema Super Gestão</h1>
-            <p>Software para gestão empresarial ideal para sua empresa.<p>
-            <div class="chamada">
-                <img src="/img/check.png">
-                <span class="texto-branco">Gestão completa e descomplicada</span>
-            </div>
-            <div class="chamada">
-                <img src="img/check.png">
-                <span class="texto-branco">Sua empresa na nuvem</span>
-            </div>
-        </div>
-
-        <div class="video">
-            <img src="img/player_video.jpg">
-        </div>
+<div class="conteudo-pagina">
+    <div class="titulo-pagina">
+        <h1>Entre em contato conosco</h1>
     </div>
 
-    <div class="direita">
-        <div class="contato">
-            <h1>Contato</h1>
-            <p>Caso tenha qualquer dúvida por favor entre em contato com nossa equipe pelo formulário abaixo.<p>
+    <div class="informacao-pagina">
+        <div class="contato-principal">
             <form>
-                <input type="text" placeholder="Nome" class="borda-branca">
+                <input type="text" placeholder="Nome" class="borda-preta">
                 <br>
-                <input type="text" placeholder="Telefone" class="borda-branca">
+                <input type="text" placeholder="Telefone" class="borda-preta">
                 <br>
-                <input type="text" placeholder="E-mail" class="borda-branca">
+                <input type="text" placeholder="E-mail" class="borda-preta">
                 <br>
-                <select class="borda-branca">
+                <select class="borda-preta">
                     <option value="">Qual o motivo do contato?</option>
                     <option value="">Dúvida</option>
                     <option value="">Elogio</option>
                     <option value="">Reclamação</option>
                 </select>
                 <br>
-                <textarea class="borda-branca">Preencha aqui a sua mensagem</textarea>
+                <textarea class="borda-preta">Preencha aqui a sua mensagem</textarea>
                 <br>
-                <button type="submit" class="borda-branca">ENVIAR</button>
+                <button type="submit" class="borda-preta">ENVIAR</button>
             </form>
         </div>
+    </div>
+</div>
+
+<div class="rodape">
+    <div class="redes-sociais">
+        <h2>Redes sociais</h2>
+        <img src="img/facebook.png">
+        <img src="img/linkedin.png">
+        <img src="img/youtube.png">
+    </div>
+    <div class="area-contato">
+        <h2>Contato</h2>
+        <span>(11) 3333-4444</span>
+        <br>
+        <span>supergestao@dominio.com.br</span>
+    </div>
+    <div class="localizacao">
+        <h2>Localização</h2>
+        <img src="img/mapa.png">
     </div>
 </div>
 </body>
