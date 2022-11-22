05637833b70115c6cafdb9ec61365fd64d26b1e6
Eduardo Esteves <eduardostevesdasilva@hotmail.com>
2022-11-22 00:22:27 -0300

    renamed migration e parameters

--- database/migrations/2022_11_22_000458_create_site_contacts_table.php
+++ database/migrations/2022_11_22_000458_create_site_contacts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSiteContatosTable extends Migration
+class CreateSiteContactsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateSiteContatosTable extends Migration
      */
     public function up()
     {
-        Schema::create('site_contatos', function (Blueprint $table) {
+        Schema::create('site_contacts', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
 
@@ -34,6 +34,6 @@ class CreateSiteContatosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('site_contatos');
+        Schema::dropIfExists('site_contacts');
     }
 }
