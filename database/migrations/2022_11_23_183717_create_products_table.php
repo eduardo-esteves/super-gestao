abaf765db5df685b460f044ba80e9db6faad07a7
Eduardo Esteves <eduardostevesdasilva@hotmail.com>
2022-11-23 20:08:40 -0300

    created two news migrations

---
+++ database/migrations/2022_11_23_183717_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('description', 100)->nullable();
+            $table->integer('pounds')->nullable();
+            $table->float('price', 8, 2)->default(0.01);
+            $table->integer('minimum_stock')->default(0);
+            $table->integer('maximum_stock')->default(1000);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
