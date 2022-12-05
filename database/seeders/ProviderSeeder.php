1c05ab76c0d5799c7de1896f30257f695d7eafc8
Eduardo Esteves <eduardostevesdasilva@hotmail.com>
2022-12-05 19:45:35 -0300

    population new ProviderSeeder

--- database/seeders/ProviderSeeder.php
+++ database/seeders/ProviderSeeder.php
@@ -13,6 +13,49 @@ class ProviderSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \DB::table('providers')->insert([
+            0 => [
+                'name'          => 'Casas Bahia',
+                'email'         => 'bahia@gmail.com',
+                'uf'            => 'BA',
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ],
+            1 => [
+                'name'          => 'Magazine Luiza',
+                'email'         => 'magazine@gmail.com',
+                'uf'            => 'SP',
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ],
+            2 => [
+                'name'          => 'Lojas CEM',
+                'email'         => 'cem@gmail.com',
+                'uf'            => 'SP',
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ],
+            3 => [
+                'name'          => 'Lojas Gearosom',
+                'email'         => 'gearosom@gmail.com',
+                'uf'            => 'PI',
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ],
+            4 => [
+                'name'          => 'Lojas Meit',
+                'email'         => 'meit@gmail.com',
+                'uf'            => 'CE',
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ],
+            5 => [
+                'name'          => 'Mil Terra',
+                'email'         => 'milterra@gmail.com',
+                'uf'            => 'PE',
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ],
+        ]);
     }
 }
