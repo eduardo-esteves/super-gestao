f533087a259f180cbd9c97207be8b8ed52dd990a
Eduardo Esteves <eduardostevesdasilva@hotmail.com>
2022-12-05 21:53:51 -0300

    simplified data population with use of new factory

--- database/seeders/SiteContactSeeder.php
+++ database/seeders/SiteContactSeeder.php
@@ -13,61 +13,6 @@ class SiteContactSeeder extends Seeder
      */
     public function run()
     {
-        \DB::table('site_contacts')->insert([
-            0 => [
-                'name'              => 'Marilu Miguel',
-                'phone'             => '(11) 97878-4541',
-                'email'             => 'marilu@gmail.com',
-                'reason_contact'    => 1,
-                'message'           => 'Erro no cadastro da plataforma',
-                'created_at'        => now(),
-                'updated_at'        => now(),
-            ],
-            1 => [
-                'name'              => 'Marta Miguel',
-                'phone'             => '(11) 97841-3541',
-                'email'             => 'marta@gmail.com',
-                'reason_contact'    => 1,
-                'message'           => 'Upload não funciona',
-                'created_at'        => now(),
-                'updated_at'        => now(),
-            ],
-            2 => [
-                'name'              => 'Eduardo Tavares',
-                'phone'             => '(11) 94741-2561',
-                'email'             => 'eduardo@gmail.com',
-                'reason_contact'    => 3,
-                'message'           => 'Fotos duplicadas',
-                'created_at'        => now(),
-                'updated_at'        => now(),
-            ],
-            3 => [
-                'name'              => 'Larissa Geovana',
-                'phone'             => '(11) 94863-2451',
-                'email'             => 'larissa@gmail.com',
-                'reason_contact'    => 2,
-                'message'           => 'Não recebi retorno de vocês',
-                'created_at'        => now(),
-                'updated_at'        => now(),
-            ],
-            4 => [
-                'name'              => 'Joana Dark',
-                'phone'             => '(21) 92574-2478',
-                'email'             => 'joana@gmail.com',
-                'reason_contact'    => 3,
-                'message'           => 'Fotos não salvam',
-                'created_at'        => now(),
-                'updated_at'        => now(),
-            ],
-            5 => [
-                'name'              => 'Patricia Silva',
-                'phone'             => '(11) 78457-2417',
-                'email'             => 'patricia@gmail.com',
-                'reason_contact'    => 2,
-                'message'           => 'Atraso na confirmação da plataforma',
-                'created_at'        => now(),
-                'updated_at'        => now(),
-            ],
-        ]);
+        \App\Models\SiteContact::factory(100)->create();
     }
 }
