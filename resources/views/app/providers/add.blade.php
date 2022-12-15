dd282cb90cd498eec2e7850ec3950ce40ba9b4ba
Eduardo Esteves <eduardostevesdasilva@hotmail.com>
2022-12-15 18:50:42 -0300

    developed two news views to add and list providers

---
+++ resources/views/app/providers/add.blade.php
@@ -0,0 +1,40 @@
+@extends('app.templates.basic')
+
+@section('title', 'Fornecedores')
+
+@section('content')
+    <div class="conteudo-pagina">
+        <div class="titulo-pagina-2">
+            <p>Fornecedor - Adicionar</p>
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="{{ route('app.providers.add') }}">Novo</a></li>
+                <li><a href="{{ route('app.providers') }}">Consulta</a></li>
+            </ul>
+        </div>
+        <div class="informacao-pagina">
+            <div style="width: 30%; margin-left: auto; margin-right: auto;">
+                <form action="" method="post">
+                    <input
+                            type="text"
+                            name="name"
+                            class="borda-preta"
+                            placeholder="Nome">
+                    <input
+                            type="text"
+                            name="email"
+                            class="borda-preta"
+                            placeholder="Email">
+                    <input
+                            type="text"
+                            name="uf"
+                            class="borda-preta"
+                            placeholder="UF">
+
+                    <button type="submit" class="borda-preta">Cadastrar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
