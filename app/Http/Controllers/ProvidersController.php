9fc871196d6c07ba139cd4e5e13d88aea5145015
Eduardo Esteves <eduardostevesdasilva@hotmail.com>
2022-12-16 08:52:02 -0300

    developed logic to save a provider

--- app/Http/Controllers/ProvidersController.php
+++ app/Http/Controllers/ProvidersController.php
@@ -3,20 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Provider;
 
 class ProvidersController extends Controller
 {
-    public function index(): \Illuminate\View\View
+    public function index(): \Illuminate\View\View|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory
     {
         return view('app.providers.index');
     }
 
-    public function add(): \Illuminate\View\View
+    public function add(Request $request): \Illuminate\View\View|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory
     {
-        return view('app.providers.add');
+        $msg_success = '';
+
+        if ($request->input('_token')) {
+            $rules = [
+                'name' => 'min:3|max:100',
+                'email' => 'email',
+                'uf'    => 'min:2|max:2',
+            ];
+
+            $msg = [
+                'name.min'      => 'Deve have ao menos 3 caracteres',
+                'name.max'      => 'Máximo de 100 caracteres',
+                'email.email'   => 'Digite um email válido',
+                'uf.min'        => 'Deve haver no minimo 2 caracteres',
+                'uf.max'        => 'Deve haver no máximo 2 caracteres',
+            ];
+
+            $request->validate($rules, $msg);
+
+            Provider::create($request->all());
+
+            $msg_success = "Cadastro realizado com sucesso!";
+        }
+
+        return view('app.providers.add', ['msg_success' => $msg_success]);
     }
 
-    public function list(): \Illuminate\View\View
+    public function list(): \Illuminate\View\View|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory
     {
         return view('app.providers.list');
     }
--- resources/views/app/providers/add.blade.php
+++ resources/views/app/providers/add.blade.php
@@ -15,22 +15,32 @@
         </div>
         <div class="informacao-pagina">
             <div style="width: 30%; margin-left: auto; margin-right: auto;">
-                <form action="" method="post">
+                {{ $msg_success }}
+                <form action="{{ route('app.providers.add') }}" method="post">
+                    @csrf
                     <input
                             type="text"
                             name="name"
                             class="borda-preta"
-                            placeholder="Nome">
+                            placeholder="Nome"
+                            value="{{ old('name') }}">
+                    {{ $errors->has('name') ? $errors->first('name') : '' }}
+
                     <input
                             type="text"
                             name="email"
                             class="borda-preta"
-                            placeholder="Email">
+                            placeholder="Email"
+                            value="{{ old('email') }}">
+                    {{ $errors->has('email') ? $errors->first('email') : '' }}
+
                     <input
                             type="text"
                             name="uf"
                             class="borda-preta"
-                            placeholder="UF">
+                            placeholder="UF"
+                            value="{{ old('uf') }}">
+                    {{ $errors->has('uf') ? $errors->first('uf') : '' }}
 
                     <button type="submit" class="borda-preta">Cadastrar</button>
                 </form>
