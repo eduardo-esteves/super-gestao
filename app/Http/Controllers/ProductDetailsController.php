2288c4ad39069b98ba0a8c74666d6de26ac9d59a
Eduardo Esteves <eduardostevesdasilva@hotmail.com>
2022-12-22 20:28:02 -0300

    implemented logic as edit as update data of view on controller

--- app/Http/Controllers/ProductDetailsController.php
+++ app/Http/Controllers/ProductDetailsController.php
@@ -26,6 +26,7 @@ class ProductDetailsController extends Controller
     public function create(): \Illuminate\View\View
     {
         $measured_units = MeasuredUnit::all();
+
         return view('app.product-details.create', [
             'measured_units' => $measured_units,
         ]);
@@ -37,7 +38,7 @@ class ProductDetailsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request): void
     {
         ProductDetail::create($request->all());
     }
@@ -56,24 +57,30 @@ class ProductDetailsController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Models\Product $product_detail
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(ProductDetail $product_detail): \Illuminate\View\View
     {
-        //
+        $measured_units = MeasuredUnit::all();
+
+        return view('app.product-details.edit', [
+            'product_detail' => $product_detail,
+            'measured_units' => $measured_units,
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Models\ProductDetail  $product
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, ProductDetail $product_detail): void
     {
-        //
+        $product_detail->update($request->all());
+        echo 'Atualização efetuada com sucesso!';
     }
 
     /**
