f264e62c4d0e5b80a2d8b86591ae6e92813e7216
Eduardo Esteves <eduardostevesdasilva@hotmail.com>
2022-12-22 20:24:18 -0300

    developed view for form edit

---
+++ resources/views/app/product-details/edit.blade.php
@@ -0,0 +1,26 @@
+@extends('app.templates.basic')
+
+@section('title', 'Detalhes do Produto')
+
+@section('content')
+    <div class="conteudo-pagina">
+        <div class="titulo-pagina-2">
+            <p>Detalhes do Produto ID: {{ $product_detail->id }}</p>
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="#">Voltar</a></li>
+            </ul>
+        </div>
+        <div class="informacao-pagina">
+            <div style="width: 30%; margin-left: auto; margin-right: auto;">
+                {{ $msg ?? '' }}
+                @component('app.product-details._components.form_create_edit', [
+                    'product_detail' => $product_detail,
+                    'measured_units' => $measured_units,
+                ])
+                @endcomponent
+            </div>
+        </div>
+    </div>
+@endsection
