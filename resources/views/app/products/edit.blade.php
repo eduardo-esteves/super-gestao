8bbf2adb17327754243e8223494b4ddcadf0d64a
Eduardo Esteves <eduardostevesdasilva@hotmail.com>
2022-12-22 14:09:41 -0300

    removed form part of views to add new component form_product

--- resources/views/app/products/edit.blade.php
+++ resources/views/app/products/edit.blade.php
@@ -16,51 +16,11 @@
         <div class="informacao-pagina">
             <div style="width: 30%; margin-left: auto; margin-right: auto;">
                 {{ $msg ?? '' }}
-                <form action="{{ route('products.update', ['product' => $product->id]) }}" method="post">
-                    @csrf
-                    @method('PUT')
-                    <input
-                        type="text"
-                        name="name"
-                        class="borda-preta"
-                        placeholder="Nome"
-                        value="{{ $product->name ?? old('name') }}">
-                    {{ $errors->has('name') ? $errors->first('name') : '' }}
-
-                    <input
-                        type="text"
-                        name="description"
-                        class="borda-preta"
-                        placeholder="Descrição"
-                        value="{{ $product->description ?? old('description') }}">
-                    {{ $errors->has('description') ? $errors->first('description') : '' }}
-
-                    <input
-                        type="text"
-                        name="pounds"
-                        class="borda-preta"
-                        placeholder="Peso"
-                        value="{{ $product->pounds ?? old('pounds') }}">
-                    {{ $errors->has('pounds') ? $errors->first('pounds') : '' }}
-
-                    <select name="measured_unit_id">
-                        <option value="0000">-- Selecione a unidade de medida --</option>
-                        @isset($measured_units)
-                            @foreach($measured_units as $measured_unit)
-                                <option
-                                    value="{{ $measured_unit->id }}"
-                                    {{ $measured_unit->id > 0
-                                        ? 'selected'
-                                        : ( old('measured_unit_id') == $measured_unit->id ? 'selected' : '' ) }}>
-                                    {{ $measured_unit->description }}
-                                </option>
-                            @endforeach
-                        @endisset
-                    </select>
-                    {{ $errors->has('measured_unit_id') ? $errors->first('measured_unit_id') : '' }}
-
-                    <button type="submit" class="borda-preta">Cadastrar</button>
-                </form>
+                @component('app._components.form_product', [
+                    'product' => $product,
+                    'measured_units' => $measured_units,
+                ])
+                @endcomponent
             </div>
         </div>
     </div>
