759b4b078f6214c7cdbdeb5ade769842366e9d70
Eduardo Esteves <eduardostevesdasilva@hotmail.com>
2022-12-29 13:33:44 -0300

    created three news migrations and yours relations with artisan tool

---
+++ database/migrations/2022_12_29_150446_create_customers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('gender', 1);
+            $table->string('occupation', 100);
+            $table->boolean('married')->default(false);
+            $table->smallInteger('age');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
---
+++ database/migrations/2022_12_29_150629_create_products_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products_orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id')->constrained();
+            $table->foreignId('product_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products_orders', function (Blueprint $table) {
+            $table->dropForeign('products_orders_customer_id_foreign');
+            $table->dropForeign('products_orders_product_id_foreign');
+        });
+        Schema::dropIfExists('products_orders');
+    }
+}
