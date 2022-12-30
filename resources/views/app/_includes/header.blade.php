80f675d7153a6a762635baa839b4b4d61e840cdf
Eduardo Esteves <eduardostevesdasilva@hotmail.com>
2022-12-30 13:44:33 -0300

    adjusted correct name route

--- resources/views/app/_includes/header.blade.php
+++ resources/views/app/_includes/header.blade.php
@@ -6,7 +6,7 @@
     <div class="menu">
         <ul>
             <li><a href="{{ route('app.home') }}">Home</a></li>
-            <li><a href="{{ route('app.customers') }}">Clientes</a></li>
+            <li><a href="{{ route('customers.index') }}">Clientes</a></li>
             <li><a href="{{ route('products.index') }}">Produtos</a></li>
             <li><a href="{{ route('app.providers') }}">Fornecedores</a></li>
             <li><a href="{{ route('app.signOut') }}">Sair</a></li>
