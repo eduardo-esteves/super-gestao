a05eec39c0132491c49ba8b0bc022597c9306d57
Eduardo Esteves <eduardostevesdasilva@hotmail.com>
2023-01-21 11:39:20 -0300

    after done rollback was added attribute table name to save in correct table name call products_orders

--- app/Models/ProductOrder.php
+++ app/Models/ProductOrder.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class ProductOrder extends Model
 {
     use HasFactory;
+    protected $table = 'products_orders';
+    protected $fillable = ['order_id', 'product_id'];
 }
